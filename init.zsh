7f9d4a42c6ca0e043bc16e7abc64b259eeabea17
laggardkernel <laggardkernel@qq.com>
2018-04-20 08:20:01 +0800

    Prezto support

---
+++ init.zsh
@@ -0,0 +1,4 @@
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+source "${0:h}/tmux.plugin.zsh"
